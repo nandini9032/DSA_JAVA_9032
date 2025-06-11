c18cf3944b12354536e00e1abf44e6b1da9f0a28
nandini9032 <nandinisamathoti0011@gmail.com>
2025-06-11 14:14:33 +0530

    Add files via upload

---
+++ linear search arr.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class linear search arr 
+{
+    public static void main(String[] args)
+    {
+        int []arr=new int [5];
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int n=sc.nextInt();
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==n)
+            {
+                System.out.println(i);
+                System.exit(0);
+            }
+            
+        }
+        System.out.println("Not found");
+    }
+}
